4272a90c5e460f77709f1f1d7d8129ff2dbc5052
cormacbeagan <mac@daysix.co>
2025-09-15 16:29:52 +0200

    Gitignore dist folder

--- dist/src/api/page-group/controllers/page-group.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * page-group controller
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreController('api::page-group.page-group');
